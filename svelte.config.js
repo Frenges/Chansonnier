27be87b14223d367ca1fee38faeee2022b88189e
Laurent Paschoud <frenges4@gmail.com>
2026-05-15 10:02:55 +0200

    chore: fix svelte.config remove assets option and use vitePreprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,17 @@
+// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.env.NODE_ENV === "development";
 
 export default {
-  preprocess: preprocess(),
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
       fallback: '404.html'
     }),
-
     paths: {
-      base: dev ? "" : "/Chansonnier",
-      assets: dev ? "" : "/Chansonnier"
+      base: dev ? "" : "/Chansonnier"
     },
-    trailingSlash: 'always'
-
-    serviceWorker: {
-      register: true
-    }
-  },
-
+  }
 };
