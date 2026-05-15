e7fd62695fe60d0e2df847de8c249dcacefba1ec
Laurent Paschoud <frenges4@gmail.com>
2026-05-15 09:53:35 +0200

    build: set base to /Chansonnier and rebuild

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,25 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.env.NODE_ENV === "development";
 
 export default {
+  preprocess: preprocess(),
   kit: {
     adapter: adapter({
       fallback: '404.html'
     }),
 
     paths: {
-      base: dev ? "" : "/Chansonnier"
+      base: dev ? "" : "/Chansonnier",
+      assets: dev ? "" : "/Chansonnier"
     },
+    trailingSlash: 'always'
 
     serviceWorker: {
       register: true
     }
   },
 
-  preprocess: [vitePreprocess()]
 };
